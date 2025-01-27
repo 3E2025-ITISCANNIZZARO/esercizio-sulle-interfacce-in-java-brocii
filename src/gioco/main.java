595d792bfad80da641739cdde30f73042e1f982f
lucia biroli <biroli.lucia@itiscannizzaro.edu.it>
2025-01-27 12:47:01 +0000

    main

--- src/gioco/main.java
+++ src/gioco/main.java
@@ -7,12 +7,15 @@ public class main {
         superEroe s1=new superEroe("spiderman");
         Vampiro v1=new Vampiro("dracula");
         Licantropo l1=new Licantropo("gino");
+        System.out.println("la forza iniziale dei personaggi è: \n supereroe: "+s1.getForza()+"\n vampiro: "+v1.getForza()+"\n licantropo: "+l1.getForza()
+        );
         s1.Combattimento();
         s1.Combattimento();
         s1.Combattimento();
         v1.morso();
         l1.attacco();
         l1.attacco();
-        System.out.println("la forza dei personaggi ora è: \n supereroe: "+s1.getForza()+"\n vampiro: "+v1.getForza()+"\n licantropo: "+l1.getForza());
+        System.out.println("la forza dei personaggi ora è: \n supereroe: "+s1.getForza()+"\n vampiro: "+v1.getForza()+"\n licantropo: "+l1.getForza()
+        );
     }
 }
